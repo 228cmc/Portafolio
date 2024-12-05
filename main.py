617d9342c0c3c9885fdb36216ee4bdf2169aa96e
cmc228 <cmc228@bath.ac.uk>
2024-12-05 13:49:43 +0000

    added new options in the main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
-from flights import add_flight, view_flights_by_criteria, update_flight
-from pilots import assign_pilot_to_flight, view_pilot_schedule
+from flights import add_flight, view_flights_by_criteria, update_flight  #import all the functions that I will use from the differnt modules
+from pilots import add_pilot, assign_pilot_to_flight, view_pilot_schedule,delete_pilot
 from destinations import manage_destinations
 
 def main():
@@ -12,11 +12,13 @@ def main():
         print("1. add a new flight")
         print("2. view flights ")
         print("3. update Flights")
-        print("4. assign pilot to flight")
-        print("5. view pilot Schedule")
-        print("6. view/Update destination information")
-        print("7. quit")
-
+        print("4. add pilot")
+        print("5. assign pilot to flight")
+        print("6. view pilot Schedule")
+        print("7. delete information of a pilot")
+        print("8. view/Update destination information")
+        print("9. quit")
+        
         choice = input("pick option: ")
 
         if choice == "1":
@@ -31,9 +33,12 @@ def main():
             assign_pilot_to_flight(cursor)
         elif choice == "6":
             view_pilot_schedule(cursor)
+
         elif choice == "7":
-            manage_destinations(cursor)
+            delete_pilot(cursor)
         elif choice == "8":
+            manage_destinations(cursor)
+        elif choice == "9":
             print("exiting...")
 
             break
